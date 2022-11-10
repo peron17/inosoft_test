5fd9d240a23c43d845b7490cfaa39bda3f3663ae
tommypria <tommypriambodo@gmail.com>
2022-11-10 17:21:00 +0700

    install passport, setup models

--- src/app/Models/Kendaraan.php
+++ src/app/Models/Kendaraan.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Kendaraan extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'tahun_keluaran', 'warna', 'harga', 'jenis_kendaraan', 'nama_kendaraan', 'spesifikasi', 'stok'
-    ];
+    protected $collection = "kendaraans";
 }
--- src/app/Models/OrderItem.php
+++ src/app/Models/OrderItem.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $collection = "order_items";
 }
