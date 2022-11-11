bfeb7750319c5cbb8845b846901b09ec8c73394a
tommypria <tommypriambodo@gmail.com>
2022-11-11 17:24:19 +0700

    update seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -18,10 +19,16 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\Kendaraan::factory(20)->create();
 
-        \App\Models\Order::factory()->count(20)->create()->each(function (\App\Models\Order $orders) {
+        $orders = \App\Models\Order::factory()->count(20)->create()->each(function (\App\Models\Order $orders) {
             \App\Models\OrderItem::factory()->count(4)->state([
                 'id_order' => $orders->id
             ])->create();
         });
+
+        foreach ($orders as $order) {
+            $items = \App\Models\OrderItem::where('id_order', $order->id)->sum('total_harga');
+            $order->total_harga = $items;
+            $order->save();
+        }
     }
 }
